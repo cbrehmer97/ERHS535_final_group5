0625027ef2a4471f5dd9075eff70f3fffea04922
Collin Brehmer <cbrehmer97@gmail.com>
2019-12-11 11:42:39 -0700

    contains final code for data to be used in plots

--- working.R
+++ working.R
@@ -16,48 +16,56 @@ dd_episodes <- jeopardy_clean %>%
   summarise(dd_count = sum(daily_double))
 
 #Function w mapping
-test1 <- jeopardy_clean %>% 
+jeopardy_final <- jeopardy_clean %>% 
+  #Change "value" of daily double to be 5 to deal with issues expanding later on
   mutate(dd_bet = case_when(daily_double == 1 ~ value,
                             daily_double == 0 ~ 0),
          value = case_when(daily_double == 1 ~ 5,
                            daily_double == 0 ~ value)) %>% 
+  #Nest data by air date
   group_by(air_date) %>% 
   nest() %>% 
+  #Determine number of categories that appaeared in each episode and filter to only episodes with all catagories asked
   mutate(categories_unique = map(data, ~ unique(select(., category))),
          categories_asked = map(data, ~ select(., round, category, value, dd_bet, daily_double)),
          num_categories = map(categories_unique, ~ nrow(.))) %>% 
   unnest(num_categories) %>% 
   filter(num_categories == 12) %>% 
+  #Assigns x position to each category and creates "perfect" data frame
   mutate(categories_unique = map(categories_unique, ~ rownames_to_column(., var = "x_pos")),
-         categories_pos = map2(categories_unique, categories_asked, ~full_join(.x, .y)),
-         categories_pos = map(categories_pos, ~ group_by(., round)),
-         categories_pos = map(categories_pos, ~ expand(., x_pos, value)))
-
-
-test2 <- test1 %>% 
-  mutate(cat_dd = map(categories_asked, ~ filter(., daily_double == 1)),
-         cat_dd = map(cat_dd, ~ select(., category)),
-         categories_test = map2(categories_asked, cat_dd, ~ right_join(.x, .y, by = "category")),
-         categories_test = map(categories_test, ~ group_by(., category)),
-         categories_test = map(categories_test, ~ mutate(., count = n(),
+         perfect_pos = map2(categories_unique, categories_asked, ~full_join(.x, .y)),
+         perfect_pos = map(perfect_pos, ~ group_by(., round)),
+         perfect_pos = map(perfect_pos, ~ expand(., x_pos, value))) %>% 
+  mutate(#Determine the categories where the DD was asked
+         categories_dd = map(categories_asked, ~ filter(., daily_double == 1)),
+         categories_dd = map(categories_dd, ~ select(., category)),
+         #Filter each episode data to only contain information about category where DD was asked
+         dd_weight = map2(categories_asked, categories_dd, ~ right_join(.x, .y, by = "category")),
+         #Weight the chance that DD was asked at y position based on number of questions asked in each category
+         dd_weight = map(dd_weight, ~ group_by(., category)),
+         dd_weight = map(dd_weight, ~ mutate(., count = n(),
                                                          weight = if_else(count == 5, 1, 1/(6-count)))),
-         test_join = map2(categories_unique, categories_pos, ~ full_join(.x, .y, by = c("x_pos"))),
-         categories_expanded = map2(test_join, categories_asked, ~ full_join(.x, .y, by = c("round", "category", "value"))),
-         categories_expanded = map2(categories_expanded, cat_dd, ~ right_join(.x, .y, by = "category")),
-         categories_test = map(categories_test, ~ select(., category, weight)),
-         categories_test = map(categories_test, ~ distinct(.)),
-         categories_expanded = map2(categories_expanded, categories_test,
+         #Full join to get x position of each category
+         categories_perfect = map2(categories_unique, perfect_pos, ~ full_join(.x, .y, by = c("x_pos"))),
+         #Make perfect data frame for daily double categories
+         dd_perfect = map2(categories_perfect, categories_asked, ~ full_join(.x, .y, by = c("round", "category", "value"))),
+         dd_perfect = map2(dd_perfect, categories_dd, ~ right_join(.x, .y, by = "category")),
+         #Filter down to what the weight should be for the questions in the DD category
+         dd_weight = map(dd_weight, ~ select(., category, weight)),
+         dd_weight = map(dd_weight, ~ distinct(.)),
+         #Add weight to places where DD could have been asked
+         dd_perfect = map2(dd_perfect, dd_weight,
                                     ~ full_join(.x, .y, by = "category")),
-         categories_expanded = map(categories_expanded, ~ mutate(., daily_double = case_when(is.na(daily_double) == TRUE ~ weight,
+         dd_perfect = map(dd_perfect, ~ mutate(., daily_double = case_when(is.na(daily_double) == TRUE ~ weight,
                                                                                              is.na(daily_double) == FALSE ~ daily_double))),
-         categories_expanded = map(categories_expanded, ~ filter(., value != 5)),
-         test_join = map(test_join, ~ filter(., value != 5)),
-         test_join = map(test_join, ~ group_by(., category)),
-         test_join = map(test_join, ~ mutate(., y_pos = c("1", "2", "3", "4", "5"))),
-         data_keep = map2(test_join, categories_expanded, ~ full_join(.x, .y, by = c("x_pos", "category", "round", "value"))))
-
-#Contains final information needed
-test_final <- test2 %>% 
+         #Remove expanded DD value of 5
+         dd_perfect = map(dd_perfect, ~ filter(., value != 5)),
+         categories_perfect = map(categories_perfect, ~ filter(., value != 5)),
+         #Add y position
+         categories_perfect = map(categories_perfect, ~ group_by(., category)),
+         categories_perfect = map(categories_perfect, ~ mutate(., y_pos = c("1", "2", "3", "4", "5"))),
+         #Combine perfect daily double and all categories into one final dataframe
+         data_keep = map2(categories_perfect, dd_perfect, ~ full_join(.x, .y, by = c("x_pos", "category", "round", "value")))) %>% 
   select(air_date, data_keep) %>% 
   unnest(cols = data_keep) %>% 
   filter(daily_double != 0) %>% 
@@ -67,12 +75,7 @@ test_final <- test2 %>%
                            x_pos == 9 | x_pos == 3 ~ 3,
                            x_pos == 10 | x_pos == 4 ~ 4,
                            x_pos == 11 | x_pos == 5 ~ 5,
-                           x_pos == 12 | x_pos == 6 ~ 6)) %>% 
-  mutate(daily_double = 1) %>% ## Added for actual values - MH
-  left_join(jeopardy_clean, by = c("air_date", "category", "daily_double", 
-                                    "round")) %>% 
-  rename("dd_value" = "value.y",
-         "value" = "value.x") 
+                           x_pos == 12 | x_pos == 6 ~ 6))
 
 ### Begining of Molly's code
 library(dplyr)
